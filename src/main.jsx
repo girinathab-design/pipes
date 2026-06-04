ad903220820151d02717b56f12532bf21b8fb5d5
girinathab-design <girinathab@gmail.com>
2026-06-04 14:22:02 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import PipesStore from "./PipesStore";
+import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <PipesStore />
+    <App />
   </React.StrictMode>
 );
